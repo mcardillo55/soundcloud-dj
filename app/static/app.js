1632ed5c3d9083d2d8e2a6830cbf45e6323adfc6
Mike Cardillo <mcardillo55@gmail.com>
2014-07-15 21:17:57 -0700

    basic concept of switching between soundcloud songs

--- app/static/app.js
+++ app/static/app.js
@@ -11,8 +11,10 @@ app = angular.module("app", [])
 app.controller('djController', [
 	"$scope",
 	"$http",
-	function($scope, $http) {
+	"$sce",
+	function($scope, $http, $sce) {
 		$scope.url = "";
+		$scope.songEmbed = $sce.trustAsHtml('<iframe width="420" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/nokturnalist/lee-foss-mk-live-the-mixmag-dj-lab-2013-06-07&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>');
 		$http.get("http://127.0.0.1:5000/api/playlist")
 		.success(function (response) {
 			$scope.songList = response.songList;
@@ -25,8 +27,13 @@ app.controller('djController', [
 			data = {
 				"url": $scope.url
 			};
-			$scope.songList.push({"id":0, "url":$scope.url});
+			$scope.songList.push({"id":$scope.songList.length+1, "url":$scope.url});
 			$http.post("http://127.0.0.1:5000/api/playlist", data)
+		};
+
+		$scope.changeSong = function (songId) {
+			var newSong =  $scope.songList[songId - 1];
+			$scope.songEmbed = $sce.trustAsHtml('<iframe width=“420” height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=' + newSong.url + '&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>');
 		};
 	}
 ]);
