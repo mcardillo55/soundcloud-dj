8511664b549374b35a797f17395e0bfca5e5e5fa
Mike Cardillo <mcardillo55@gmail.com>
2014-07-16 01:05:53 -0700

    remove references to wtforms. add requirements file

--- app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import TextField
-from wtforms.validators import Required
-
-
-class SubmissionForm(Form):
-    subURL = TextField('subURL', validators=[Required()])
--- app/views.py
+++ app/views.py
@@ -2,18 +2,12 @@ import urllib2
 import json
 from flask import render_template, request, jsonify
 from app import app, db
-from forms import SubmissionForm
 from models import Song
 
 
 @app.route('/', methods=['GET', 'POST'])
-def hello():
-    form = SubmissionForm()
-    if form.validate_on_submit():
-        db.session.add(Song(url=form.data['subURL']))
-        db.session.commit()
-        return "Success!"
-    return render_template("index.html", form=form, songs=Song.query.all())
+def index():
+    return render_template("index.html", songs=Song.query.all())
 
 
 @app.route('/api/playlist', methods=['GET', 'POST'])
