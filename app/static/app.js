cbe19c94db6f4ce55ffb3eb9afd0b5b4c9ee3336
Mike Cardillo <mcardillo55@gmail.com>
2014-07-19 02:56:13 -0700

    enabled continous play for when youtube videos end

--- app/static/app.js
+++ app/static/app.js
@@ -23,8 +23,6 @@ app.controller('djController', [
 			}
 		})
 
-
-
 		$scope.addSong = function () {
 			newUrl = $scope.url;
 			if (newUrl.indexOf("soundcloud") > -1 || newUrl.indexOf("youtube") > -1) {
@@ -42,23 +40,31 @@ app.controller('djController', [
 			newSong =  $scope.songList[songId - 1];
 			if (newSong.url.indexOf("soundcloud") > -1) {
 				$scope.playerUrl = $sce.trustAsResourceUrl('https://w.soundcloud.com/player/?url=' + newSong.url + '&auto_play=true&visual=true');
+				//temporary? hack to bind soundcloud listener after DOM update
+				$timeout(function() { 
+					SC.Widget(myPlayer).bind(SC.Widget.Events.FINISH, 
+						function() { 
+							rand = Math.floor(Math.random() * $scope.songList.length);
+							$scope.changeSong($scope.songList[rand].id);
+							$scope.$apply();
+						})
+					}, 1000);
 			} else if (newSong.url.indexOf("youtube") > -1) {
-				$scope.playerUrl = $sce.trustAsResourceUrl(newSong.url + "?autoplay=1");
+				$scope.playerUrl = $sce.trustAsResourceUrl(newSong.url + "?enablejsapi=1&autoplay=1&origin=http://127.0.0.1:5000");
+				//temporary? hack to bind youtube listener after DOM update
+				$timeout(function() {
+					new YT.Player('myPlayer').addEventListener('onStateChange', function(event){
+						if (event.data == YT.PlayerState.ENDED) {
+							rand = Math.floor(Math.random() * $scope.songList.length);
+							$scope.changeSong($scope.songList[rand].id);
+							$scope.$apply();
+						}
+					});
+				}, 1000);
 			}
 			$scope.curSongId = songId;
-			
-			//temporary? hack to bind listener after DOM update
-			$timeout(function() { 
-				SC.Widget(myPlayer).bind(SC.Widget.Events.FINISH, 
-					function() { 
-						//do {
-							rand = Math.floor(Math.random() * $scope.songList.length);
-						//} while (rand != $scope.curSongId);
-						$scope.changeSong($scope.songList[rand].id);
-						$scope.$apply();
-					})
-				}, 5000);
-
 		};
 	}
 ]);
+
+
