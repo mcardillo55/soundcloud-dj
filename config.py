836b953be4f247c048e504865b07c5d3d4fa8ef7
Mike Cardillo <mcardillo55@gmail.com>
2017-02-16 12:03:10 -0800

    Moved location of the SQLite database to allow database to be in a group-writable directory without forcing the rest of the app to also be in one.␘

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db/app.db')
 CSRF_ENABLED = True
 SECRET_KEY = 'this is a secret'
 HOSTNAME = "http://127.0.0.1:5000"
