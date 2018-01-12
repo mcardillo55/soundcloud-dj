b40666315fdda01ab36044b58c5d25acdf1011ac
Mike Cardillo <mcardillo55@gmail.com>
2018-01-11 21:39:08 -0800

    Direct access to the group was getting capcha'ed. Navigate to main site then group site now

--- tools/facebook.js
+++ tools/facebook.js
@@ -3,14 +3,21 @@ Navigate to the desired facebook page and it will log
 in with the provided credentials and dump the interpreted
 html to console */
 var casper = require('casper').create({verbose: false, logLevel: 'debug'});
+casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7');
 
-casper.start('https://www.facebook.com/groups/518171768298214/', function() {
+casper.start('https://www.facebook.com/', function() {
     this.waitForSelector('form#login_form');
 });
 
 casper.then(function() {
     this.fill('form#login_form', {'email': '', 'pass': ''}, true);
 });
+
+casper.then(function() {
+    this.waitForSelector('body');
+});
+
+casper.thenOpen('https://www.facebook.com/groups/518171768298214/');
 
 casper.then(function() {
     this.waitForSelector('div#contentArea');
