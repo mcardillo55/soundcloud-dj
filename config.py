ec15dea49361d217a0afee80b2c659ad20315308
Mike Cardillo <mike@mjcardillo.com>
2020-06-16 18:02:02 -0700

    Updated db creation script

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
+DB_PATH = "db/app.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db/app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, DB_PATH)
 CSRF_ENABLED = True
 SECRET_KEY = 'this is a secret'
 HOSTNAME = "http://127.0.0.1:5000"
--- db_create.py
+++ db_create.py
@@ -1,5 +1,11 @@
-from config import SQLALCHEMY_DATABASE_URI
+from config import DB_PATH
 import os.path
 from app import db
+
+try:
+    os.makedirs(os.path.split(DB_PATH)[0])
+except:
+    print("DB_PATH already exists, not creating...")
+    pass
 
 db.create_all()
